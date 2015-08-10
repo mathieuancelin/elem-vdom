562099b11fbfdcf8f210e9c0ec2d3ca4995667fa
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2015-08-10 22:46:48 +0200

    Add slider

--- examples/sink/fps.js
+++ examples/sink/fps.js
@@ -50,12 +50,12 @@ Showcase.registerTile('Bubbles ...', container => {
     if (!bubbles) {
       bubbles = [];
       for (let i = 0; i < bubblesCount; i++) {
-        bubbles.push({ x: rnd(600), y: rnd(600), r: rnd(50), speed: baseSpeed, incrementX: baseSpeed, incrementY: baseSpeed, color: colors[parseInt(rnd(6), 10) - 1] });
+        bubbles.push({ x: rnd(600), y: rnd(600), r: 10 + rnd(40), speed: baseSpeed, incrementX: baseSpeed, incrementY: baseSpeed, color: colors[parseInt(rnd(6), 10) - 1] });
       }
     }
     for (let i in bubbles) {
       let bubble = bubbles[i];
-      bubble.speed = baseSpeed + (Math.random() * 5);
+      bubble.speed = baseSpeed + rnd(5);
       if (bubble.x <= bubble.r) {
         bubble.incrementX = 0 + bubble.speed;
       } else if (bubble.x >= (600 - bubble.r)) {
@@ -70,9 +70,25 @@ Showcase.registerTile('Bubbles ...', container => {
       bubble.y = bubble.y + bubble.incrementY;
     }
     requestAnimationFrame(() => this.setState({ bubbles: [...bubbles] }));
-    return Elem.svg('svg', { xmlns: Elem.svgNS, version: '1.1', width: '600', height: '600', style: { borderStyle: 'solid', borderColor: 'black', height: '600px', width: '600px' } },
-      bubbles.map(bubble => Elem.el(Bubble, bubble))
-    );
+    const changeBubbles = (number) => {
+      let currentBubbles = [...this.state.bubbles];
+      if (number < currentBubbles.length) {
+        for (let i = 0; i < (currentBubbles.length - number); i++) {
+          currentBubbles.pop();
+        }
+      } else if (number > currentBubbles.length) {
+        for (let i = 0; i < (number - currentBubbles.length); i++) {
+          currentBubbles.push({ x: rnd(600), y: rnd(600), r: 10 + rnd(40), speed: baseSpeed, incrementX: baseSpeed, incrementY: baseSpeed, color: colors[parseInt(rnd(6), 10) - 1] });
+        }
+      }
+      this.setState({ bubbles: currentBubbles, number });
+    };
+    return Elem.el('div', [
+      Elem.el('input', { type: 'range', min: '4', max: '10', value: this.state.number || '4', onChange: (e) => changeBubbles(e.target.value) }),
+      Elem.svg('svg', { xmlns: Elem.svgNS, version: '1.1', width: '600', height: '600', style: { borderStyle: 'solid', borderColor: 'black', height: '600px', width: '600px' } },
+        bubbles.map(bubble => Elem.el(Bubble, bubble))
+      )
+    ]);
   }
 
   Elem.render(Bubbles, container);
