dba24e76b3e93329dfa4a055ba5efd5d71d64a66
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2015-08-13 12:01:16 +0200

    Basic test use iojs and pass

--- tests/basic.spec.js
+++ tests/basic.spec.js
@@ -1,24 +1,23 @@
-import * as Elem from '../lib/main';
-import chai from 'chai';
-import setupEnv from './utils/dom';
-
-const expect = chai.expect;
+const chai = require('chai');
+const setupEnv = require('./utils/dom');
 
 setupEnv();
 
+const Elem = require('../lib/main');
+
+const expect = chai.expect;
+
 function SimpleComponent() {
-  return Elem.el('h1', { id: 'title'}, 'Hello World!');
+  return Elem.el('h1', 'Hello World!');
 }
 
 describe('A simple component', () => {
   it('can be rendered in the DOM', done => {
     Elem.render(SimpleComponent, '#app');
-
-    console.log(window.document.innerHTML);
-
-    let node = document.getElementById('#title');
+    let node = document.getElementsByTagName('h1')[0];
     expect(node).to.exist;
     expect(node.innerHTML).to.be.a('string');
     expect(node.innerHTML).to.be.equal('Hello World!');
+    done();
   });
 });
