bc370804b9354b32e46f96d9c2f75f0cfe8a8a27
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2015-08-19 12:12:19 +0200

    Add build without devtools

---
+++ small.sh
@@ -0,0 +1,4 @@
+rm -rf ./small-lib
+cp -r ./lib ./small-lib
+rm -rf ./small-lib/devtools
+mv ./small-lib/small ./small-lib/devtools
---
+++ webpack.config.small.js
@@ -0,0 +1,34 @@
+var webpack = require('webpack');
+
+module.exports = {
+  output: {
+    path: './dist/',
+    publicPath: '/assets/',
+    filename: '[name].js',
+    library: 'Elem',
+    libraryTarget: 'umd'
+  },
+  entry: {
+    'elem.prod': ['./small-lib/main.js']
+  },
+  resolve: {
+    extensions: ['', '.js']
+  },
+  module: {
+    loaders: []
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      '__DEV__': process.env.NODE_ENV === 'production' ? false : true,
+      '__DEVTOOLS__': process.env.NODE_ENV === 'production' ? false : true,
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        screw_ie8: true,
+        warnings: false
+      }
+    })
+  ]
+};
