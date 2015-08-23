e13ef305b00a4b7bef1a87b52fa153acd2542cf5
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2015-08-23 16:30:19 +0200

    Use new fluent api on some tests

--- tests/jsx.spec.js
+++ tests/jsx.spec.js
@@ -2,25 +2,25 @@
 const chai = require('chai');
 const Elem = require('../src/main');
 const DOM = require('../src/test/dom');
-const Tests = require('../src/test/desc');
+const it = require('../src/test/desc').it;
 const expect = chai.expect;
 
 describe('elem-vdom JSX support', () => {
 
-  Tests.it('can render a simple JSX tree', () => {
+  it('can render a simple JSX tree', () => {
 
     function SimpleComponent() {
       return <h1>Hello World!</h1>;
     }
 
     DOM.renderComponent(SimpleComponent);
-    let node = DOM.node('h1');
-    expect(node).to.exist;
-    expect(node.innerHTML).to.be.a('string');
-    expect(node.innerHTML).to.be.equal('Hello World!');
+    DOM.select('h1')
+      .shouldExist()
+      .html()
+        .shouldBe('Hello World!');
   });
 
-  Tests.it('can render multiples arrays with flattening', () => {
+  it('can render multiples arrays with flattening', () => {
 
     function Index() {
       return (
@@ -37,17 +37,20 @@ describe('elem-vdom JSX support', () => {
     }
 
     DOM.renderComponent(Index);
-    let node = DOM.node('#list');
+
+    DOM.select('#list')
+      .shouldExist()
+      .className().shouldBe('theList')
+      .id().shouldBe('list')
+      .children().count().shouldBe(5)
+      .html()
+        .shouldBe('<li id="zero">0</li><li id="1">1</li><li id="2">2</li><li id="3">3</li><li id="4">4</li>');
+
     let lis = DOM.nodes('li');
-    expect(node.className).to.be.equal('theList');
-    expect(node.id).to.be.equal('list');
-    expect(node).to.exist;
-    expect(node.childNodes.length).to.be.equal(5);
     expect(lis.length).to.be.equal(5);
-    expect(node.innerHTML).to.be.equal('<li id="zero">0</li><li id="1">1</li><li id="2">2</li><li id="3">3</li><li id="4">4</li>');
   });
 
-  Tests.it('can render component with children', () => {
+  it('can render component with children', () => {
 
     function Wrapper() {
       return <div className="wrapper" id={this.props.id}>{this.props.children}</div>;
@@ -69,14 +72,23 @@ describe('elem-vdom JSX support', () => {
     }
 
     DOM.renderComponent(Component);
-    let component = DOM.node('#component');
-    let theWrapper = DOM.node('#theWrapper');
-    expect(component).to.exist;
-    expect(theWrapper).to.exist;
-    expect(component.childNodes.length).to.be.equal(1);
-    expect(theWrapper.childNodes.length).to.be.equal(2);
-    expect(component.innerHTML).to.be.equal('<div class="wrapper" id="theWrapper"><span>Element</span><span>Element</span></div>');
-    expect(theWrapper.innerHTML).to.be.equal('<span>Element</span><span>Element</span>');
+
+    DOM.select('#component')
+      .shouldExist()
+      .children()
+        .count()
+          .shouldBe(1)
+      .html()
+        .shouldBe('<div class="wrapper" id="theWrapper"><span>Element</span><span>Element</span></div>');
+
+    DOM.select('#theWrapper')
+      .shouldExist()
+      .children()
+        .count()
+          .shouldBe(2)
+      .html()
+        .shouldBe('<span>Element</span><span>Element</span>');
+
   });
 
 });
